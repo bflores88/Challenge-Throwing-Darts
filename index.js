24e0b26186e95ff79e8743183615a763c93d7ddd
kmietotwy <kmietotwy@gmail.com>
2019-04-18 09:12:05 -1000

    dart calculator function

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+'use strict';
+
 function scoreCalculator(arr) {
   // do work here
+  let score = 0;
+
+  if (arr.length === 0) {
+    score = -1;
+  }
+
+  arr.forEach((elem) => {
+    if (elem < 5) {
+      score += 10;
+    } else if (elem >= 5 && elem <= 10) {
+      score += 5;
+    }
+  });
+
+  return score;
 }
 
 module.exports = scoreCalculator;
