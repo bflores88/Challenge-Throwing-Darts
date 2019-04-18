18a2d504e370ef173cd20ecd0f847e4c70e24fd0
kmietotwy <kmietotwy@gmail.com>
2019-04-18 09:37:10 -1000

    formatting

--- test/spec.js
+++ test/spec.js
@@ -6,29 +6,28 @@ const chai = require('chai');
 const should = chai.should();
 const expect = chai.expect;
 
-describe ('Darts Score Calculator', function () {
-
-  it('should be a function', function (){
+describe('Darts Score Calculator', function() {
+  it('should be a function', function() {
     scoreThrows.should.be.a('function');
   });
 
-  it('should return a number', function (){
+  it('should return a number', function() {
     expect(scoreThrows([])).to.be.a('number');
   });
 
-  it('an empty collection should return -1', function (){
+  it('an empty collection should return -1', function() {
     scoreThrows([]).should.equal(-1);
   });
 
-  it('if all radiuses are less than 5, award 100 bonus points!', function (){
+  it('if all radiuses are less than 5, award 100 bonus points!', function() {
     scoreThrows([1, 1, 1]).should.equal(330);
   });
 
-  it('should accept a collection of scores of any length', function (){
-    scoreThrows([ 1, 7, 2, 9, 14, 3, 20]).should.equal(40);
+  it('should accept a collection of scores of any length', function() {
+    scoreThrows([1, 7, 2, 9, 14, 3, 20]).should.equal(40);
   });
 
-  it('should only accept scores between 1 and 20', function (){
+  it('should only accept scores between 1 and 20', function() {
     scoreThrows([-1, 1, 7, 25]).should.equal(15);
   });
 });
